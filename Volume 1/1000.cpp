8881516d055e1b8db25d55e3a89ad4a34550832c
Andrei Mihai Ciobanu <89391344+amcbn06@users.noreply.github.com>
2024-12-09 16:41:12 +0200

    Create 1000.cpp

---
+++ Volume 1/1000.cpp
@@ -0,0 +1,19 @@
+/* 1000. A + B
+
+Strategy:
+Just summate the numbers
+
+* Time complexity: O(1)
+* Space complexity: O(1)
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+long long a, b;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin >> a >> b;
+	cout << a + b;
+}
